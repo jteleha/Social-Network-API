dd0eeb775b2cda5b846f198f98361beb2c489573
jteleha <telehajoel@gmail.com>
2024-05-04 23:16:44 -0400

    updated thoughtController.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,66 @@
+const Thought = require('../models/Thought');
+
+async function getAllThoughts(req, res) {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function getThoughtById(req, res) {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function createThought(req, res) {
+  try {
+    const thought = await Thought.create(req.body);
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function updateThought(req, res) {
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(updatedThought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function deleteThought(req, res) {
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+    if (!deletedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json({ message: 'Thought deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+module.exports = {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+};
